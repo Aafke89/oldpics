387381793233da6c802c0817e8c8c54095795d78
Aafke <aafke@oberon.nl>
2016-11-21 17:58:16 +0100

    Fixed key

--- src/AppBundle/Service/FileUploader.php
+++ src/AppBundle/Service/FileUploader.php
@@ -9,36 +9,32 @@
 namespace AppBundle\Service;
 
 
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class FileUploader
 {
     private $targetDir;
 
-    public function __construct($targetDir)
+    /** @var ContainerInterface */
+    protected $container;
+
+    public function __construct($targetDir, ContainerInterface $container)
     {
         $this->targetDir = $targetDir;
+        $this->container = $container;
 
     }
 
     public function upload(UploadedFile $file)
     {
         \Cloudinary::config([
-            "cloud_name" => "hqpsx2eiv",
-            "api_key" => "991218623932461",
-            "api_secret" => "5_cjYYcO6wm1if2HotsZ57QGAzs"
+            "cloud_name" => $this->container->getParameter('cloudinary_name'),
+            "api_key" => $this->container->getParameter('cloudinary_api_key'),
+            "api_secret" => $this->container->getParameter('cloudinary_secret_api')
         ]);
 
-        $extension = $file->guessExtension();
-        $fileName = md5(uniqid()).'.'.$extension;
-
         $cloudinary_array = \Cloudinary\Uploader::upload("$file");
-
-//
-//        $file->move(
-//            $this->targetDir, $fileName );
-
-
 
         return $cloudinary_array["url"];
     }
