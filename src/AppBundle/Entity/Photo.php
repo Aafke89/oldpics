101be559da1217e54637238afb85cdfc6d94373e
Aafke <aafke@oberon.nl>
2016-11-23 14:23:27 +0100

    large images

--- src/AppBundle/Entity/Photo.php
+++ src/AppBundle/Entity/Photo.php
@@ -40,7 +40,7 @@ class Photo
     /**
      * @ORM\Column(type="string")
      * @Assert\File(
-     *     maxSize = "2048k",
+     *     maxSize = "3000k",
      *     mimeTypes = {"image/jpeg", "image/png"},
      *     mimeTypesMessage = "Please upload a valid image"
      * )
