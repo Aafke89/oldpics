a13f674d00981b2eccad081b4b9a703a24573956
Aafke <aafke@oberon.nl>
2016-11-23 13:05:54 +0100

    ip_whitelisting one more time

--- src/AppBundle/Controller/MainController.php
+++ src/AppBundle/Controller/MainController.php
@@ -11,7 +11,6 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\BrowserKit\Request;
 
 class MainController extends Controller
 {
